6fa9e0e4a594c0e077f28b1ffad5afee9dbf81ed
Roberto <vlad.drahnea@gmail.com>
2019-12-04 12:17:11 +0200

    changed second interpolation to format way too

--- capitals.py
+++ capitals.py
@@ -10,6 +10,6 @@ def letters_count():
         elif (i.isupper()):
             count_upper = count_upper + 1
     print('There are {} uppercase letters'.format(count_upper))
-    print('There are', count_lower, ' lowercase letters')
+    print('There are {} lowercase letters'.format.(count_lower))
 
 letters_count()
